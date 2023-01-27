4d0e21e5a188ff644ca30f70acb6c3c7f24e9e6a
Rafael Silva Santos <rafael.silva.santos_@hotmail.com>
2023-01-27 12:33:46 -0300

    Conexão com o MongoDB

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
+const { MongoClient } = require("mongodb")
 const express = require("express")
-const app = express()
+let db
 
-app.get("/", (req, res) => {
-    res.send("Homepage")
+const app = express()
+/*
+npm install ejs
+app.set("view engine", "ejs")
+app.set("views", "./views")
+*/
+
+//News
+app.get("/", async (req, res) => {
+    const allAnimals = await db.collection("news").find().toArray()
+    //res.render("home")
 })
 
 app.get("/admin", (req, res) => {
     res.send("Admin page")
 })
 
-app.listen(3000)
+async function start () {
+    //Connect and Start
+    const client = new MongoClient("mongodb://root:root@localhost:27017/NewsApp?&authSource=admin")
+    await client.connect()
+    db = client.db()
+    app.listen(3000)
+}
+start()
